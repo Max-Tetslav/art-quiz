d3402bd268dc0b9279b94df645f8f3a6239f82d9
Max Teclav <maks.tetslav@yandex.ru>
2021-11-12 00:08:15 +0300

    feat: implement open/close settings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.scss';
+
+const OPEN_SETTINGS_BTN_EL = document.querySelector('.settings-btn');
+const CLOSE_SETTINGS_BTN_EL = document.querySelector('.back-btn');
+const SETTINGS_CONTAINER_EL = document.querySelector('.settings__container');
+
+function toggleSettingsMenu() {
+	SETTINGS_CONTAINER_EL.classList.toggle('show');
+}
+
+OPEN_SETTINGS_BTN_EL.addEventListener('click', toggleSettingsMenu);
+CLOSE_SETTINGS_BTN_EL.addEventListener('click', toggleSettingsMenu);
